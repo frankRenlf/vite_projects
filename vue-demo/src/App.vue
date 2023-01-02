a1425f0ca84ef33247e0742fd98cfd07a4656ae9
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-02 22:36:34 +0800

    Update App.vue

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+let a:number=10;
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    {{ a }}
+  </div>
 </template>
 
 <style scoped>
