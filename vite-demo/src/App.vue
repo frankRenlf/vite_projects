3f345f37f6651b0b0c4a377559e7abf7b3b49d4e
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-03 10:14:21 +0800

    Update App.vue

--- vite-demo/src/App.vue
+++ vite-demo/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {ref} from "vue";
 let a=ref(5);
+let b = ref('')
 const del = ()=>{
   a.value--;
 }
@@ -8,6 +9,8 @@ const del = ()=>{
 
 <template>
   <div>
+    <input type="text" v-model="b">
+    <h2>{{b}}</h2>
     <h1 v-show="a">{{a}}</h1>
     <button @click="del">del</button>
   </div>
