bdd67761c2cf01f0282bd9f3b81dc6cbfb70cace
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-09 16:16:33 +0800

    add

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
-import { reactive, ref, watch, watchEffect } from "vue";
+import {reactive, ref, watch, watchEffect} from "vue";
 import IndexComponent from "@/components/layout/IndexComponent.vue";
+
+let obj = reactive({
+  name: "frank",
+  age: 23,
+});
+let list1 = reactive({
+  address:'',
+});
+let ret = (obj2: object) => {
+  console.log(obj2);
+  list1.address = obj2.address;
+}
 </script>
 
 <template>
-  <IndexComponent></IndexComponent>
+  <div>{{ list1 }}</div>
+  <IndexComponent :obj1="obj" @on-click1="ret"></IndexComponent>
 </template>
 
 <style lang="less">
--- vue-demo/src/components/layout/IndexComponent.vue
+++ vue-demo/src/components/layout/IndexComponent.vue
@@ -1,4 +1,6 @@
 <template>
+  <div>{{ obj1 }} ------</div>
+  <button @click="clickTap">emit</button>
   <div>
     <div>
       <MenuComponent></MenuComponent>
@@ -14,8 +16,28 @@
 import HeaderComponent from "@/components/layout/HeaderComponent.vue";
 import MenuComponent from "@/components/layout/MenuComponent.vue";
 import ContentComponent from "@/components/layout/ContentComponent.vue";
+import { reactive } from "vue";
+
+defineProps<{
+  obj1: object;
+}>();
+// const emit = defineEmits<{
+//   (
+//     e: "on-click",
+//     obj2: {
+//       address: "hz";
+//       current: "2023";
+//     }
+//   ): void;
+// }>();
+let obj2 = reactive({
+  address: "hz",
+  current: "2023",
+});
+const emit = defineEmits(["on-click1"]);
+const clickTap = () => {
+  emit("on-click1", obj2);
+};
 </script>
 
-<style scoped lang="less">
-
-</style>
+<style scoped lang="less"></style>
