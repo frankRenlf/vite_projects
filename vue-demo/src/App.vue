df009d42d3ddc754a73a60515f2e877e65a62bc7
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-10 16:52:31 +0800

    customRef

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,49 +1,44 @@
 <template>
   <h2>App</h2>
-  <h3>foo: {{ stateAsRefs.foo }}</h3>
-  <h3>bar: {{ stateAsRefs.bar }}</h3>
-  <h3>foo2: {{ foo }}</h3>
-  <h3>bar2: {{ bar }}</h3>
-  <input type="text" ref="ip">
-  <text-one></text-one>
+  <input v-model="keyword" placeholder="搜索关键字" />
+  <p>{{ keyword }}</p>
 </template>
 
 <script setup lang="ts">
-import {onMounted, provide, reactive, ref, toRefs} from "vue";
-import TextOne from "@/components/TextOne.vue";
 /*
-toRefs:
-  将响应式对象中所有属性包装为ref对象, 并返回包含这些ref对象的普通对象
-  应用: 当从合成函数返回响应式对象时，toRefs 非常有用，
-        这样消费组件就可以在不丢失响应式的情况下对返回的对象进行分解使用
+customRef:
+  创建一个自定义的 ref，并对其依赖项跟踪和更新触发进行显式控制
+
+需求: 
+  使用 customRef 实现 debounce 的示例
 */
-const ip = ref<HTMLElement>();
 
-onMounted(() => {
-  ip.value && ip.value.focus()
-})
-const state = reactive({
-  foo: "a",
-  bar: "b",
-});
-provide("test1", "from app");
-const stateAsRefs = toRefs(state);
+import { ref, customRef } from "vue";
 
-setTimeout(() => {
-  state.foo += "++";
-  state.bar += "++";
-}, 2000);
+const keyword = useDebouncedRef("", 500);
+console.log(keyword);
 
-const {foo, bar} = useReatureX();
-
-function useReatureX() {
-
-
-  setTimeout(() => {
-    state.foo += "++";
-    state.bar += "++";
-  }, 2000);
-
-  return toRefs(state);
+/* 
+实现函数防抖的自定义ref
+*/
+function useDebouncedRef<T>(value: T, delay = 200) {
+  let timeout: number;
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        // 告诉Vue追踪数据
+        track();
+        return value;
+      },
+      set(newValue: T) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          value = newValue;
+          // 告诉Vue去触发界面更新
+          trigger();
+        }, delay);
+      },
+    };
+  });
 }
 </script>
