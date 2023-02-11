31b411532e0d005b69a643760070296809a758f0
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-11 15:49:57 +0800

    Update App.vue

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { computed, isRef, onMounted, ref } from "vue";
 import TodoItem from "@/components/TodoItem.vue";
 
 const newTodoText = ref("");
@@ -27,18 +27,35 @@ function addNewTodo() {
   });
   newTodoText.value = "";
 }
+
+const numbers = ref([1, 2, 3, 4, 5]);
+
+const evenNumbers = computed(() => {
+  return numbers.value.filter((n) => n % 2 === 0);
+});
+console.log(numbers.value, isRef(numbers));
+console.log(evenNumbers.value, isRef(evenNumbers));
+const btn1 = (msg, event) => {
+  console.log(event);
+};
+let ip = ref();
+onMounted(() => {
+  ip.value.focus();
+});
 </script>
 
 <template>
-  <form v-on:submit.prevent="addNewTodo">
+  <div>
     <label for="new-todo">Add a todo</label>
     <input
+      ref="ip"
       v-model="newTodoText"
       id="new-todo"
       placeholder="E.g. Feed the cat"
+      @keyup.enter.prevent="addNewTodo"
     />
-    <button>Add</button>
-  </form>
+    <button @click="addNewTodo">Add</button>
+  </div>
   <ul>
     <todo-item
       v-for="(todo, index) in todos"
@@ -47,4 +64,5 @@ function addNewTodo() {
       @remove="todos.splice(index, 1)"
     ></todo-item>
   </ul>
+  <button @click="btn1('test', $event)">btn1</button>
 </template>
