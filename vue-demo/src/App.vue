d62a234697644f4ba54af5e37f7181804bf46dbd
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-11 18:04:55 +0800

    add

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,110 +1,25 @@
 <script setup>
-import { computed, isRef, onMounted, ref } from "vue";
-import TodoItem from "@/components/TodoItem.vue";
+import { onMounted, ref } from "vue";
+import ChildCom from "@/components/ChildCom.vue";
 
-const newTodoText = ref("");
-const todos = ref([
-  {
-    id: 1,
-    title: "Do the dishes",
-  },
-  {
-    id: 2,
-    title: "Take out the trash",
-  },
-  {
-    id: 3,
-    title: "Mow the lawn",
-  },
-]);
-
-let nextTodoId = 4;
-
-function addNewTodo() {
-  todos.value.push({
-    id: nextTodoId++,
-    title: newTodoText.value,
-  });
-  newTodoText.value = "";
-}
-
-const numbers = ref([1, 2, 3, 4, 5]);
-
-const evenNumbers = computed(() => {
-  return numbers.value.filter((n) => n % 2 === 0);
-});
-console.log(numbers.value, isRef(numbers));
-console.log(evenNumbers.value, isRef(evenNumbers));
-const btn1 = (msg, event) => {
-  console.log(event);
-};
-let ip = ref();
+const ip = ref();
+const cc = ref();
 onMounted(() => {
-  ip.value.focus();
+  console.log(ip.value);
+  console.log(cc.value.a);
+  cc.value.a=2;
+  cc.value.b="aaa";
 });
-let checked = ref([]);
-let picked = ref("One");
-let select1 = ref();
-let select2 = ref([]);
-const options = ref([
-  { text: "One", value: "A" },
-  { text: "Two", value: "B" },
-  { text: "Three", value: "C" },
-]);
-let toggle = ref(false);
 </script>
 
 <template>
-  <input type="checkbox" v-model="toggle" true-value="yes" false-value="no" />
-  <div>{{ toggle }}</div>
-  <input type="checkbox" v-model="toggle" />
-  <div>Selected: {{ select2 }}</div>
-  <select v-model="select2" multiple>
-    <option v-for="option in options" :value="option.value" :key="option.value">
-      {{ option.text }}
-    </option>
-  </select>
-
-  <div>Selected: {{ select1 }}</div>
-  <select name="" id="" v-model="select1">
-    <option>a</option>
-    <option>b</option>
-    <option>c</option>
-  </select>
-
-  <div>Picked: {{ picked }}</div>
-
-  <input type="radio" value="One" v-model="picked" />
-  <label for="one">One</label>
-
-  <input type="radio" value="Two" v-model="picked" />
-  <label for="two">Two</label>
-  <h2>{{ checked }}</h2>
-  <input type="checkbox" id="checkbox" value="1" v-model="checked" />
-  <label for="a">a</label>
-  <input type="checkbox" id="checkbox" value="2" v-model="checked" />
-  <label for="b">b</label>
-  <input type="checkbox" id="checkbox" value="3" v-model="checked" />
-  <label for="c">c</label>
-  <div>
-    <label for="new-todo">Add a todo</label>
-    <input
-      type="number"
-      ref="ip"
-      v-model.trim="newTodoText"
-      id="new-todo"
-      placeholder="E.g. Feed the cat"
-      @keyup.enter.prevent="addNewTodo"
-    />
-    <button @click="addNewTodo">Add</button>
-  </div>
-  <ul>
-    <todo-item
-      v-for="(todo, index) in todos"
-      :key="todo.id"
-      :title="todo.title"
-      @remove="todos.splice(index, 1)"
-    ></todo-item>
-  </ul>
-  <button @click="btn1('test', $event)">btn1</button>
+  <input
+    type="text"
+    :ref="
+      (el) => {
+        ip = el;
+      }
+    "
+  />
+  <child-com ref="cc"></child-com>
 </template>
---
+++ vue-demo/src/components/ChildCom.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{}}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+let a = 1;
+const b = ref("fff");
+defineExpose({
+  a,
+  b,
+});
+</script>
+
+<style scoped></style>
