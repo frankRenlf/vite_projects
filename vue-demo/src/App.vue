46cd946bfa4b39db073cbf355c64dc291d0a835b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-11 20:57:30 +0800

    add

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,31 +1,80 @@
+<!--
+这个示例会通过 GitHub 的 API 获取最新的 Vue.js 提交信息并将其展示为列表。
+你可以在两个分支之间切换。
+-->
+
 <script setup>
-import { ref } from "vue";
-import BlogPost from "@/components/BlogPost.vue";
+import { ref, watchEffect } from "vue";
 
-const posts = ref([
-  { id: 1, title: "My journey with Vue" },
-  { id: 2, title: "Blogging with Vue" },
-  { id: 3, title: "Why Vue is so fun" },
-]);
+const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`;
+const branches = ["main", "v2-compat"];
 
-const postFontSize = ref(1);
+const currentBranch = ref(branches[0]);
+const commits = ref(null);
+watchEffect(async () => {
+  // 该 effect 会立即运行，
+  // 并且在 currentBranch.value 改变时重新运行
+  const url = `${API_URL}${currentBranch.value}`;
+  commits.value = JSON.parse(
+    await fetch(url).then(function (data) {
+      // text()方法属于fetchAPI的一部分，它返回一个Promise实例对象，
+      // 用于获取后台返回的数据 return data.text();
+      return data.text();
+    })
+  );
+  console.log(commits.value === (await (await fetch(url)).json()));
+});
+
+function truncate(v) {
+  const newline = v.indexOf("\n");
+  return newline > 0 ? v.slice(0, newline) : v;
+}
+
+function formatDate(v) {
+  return v.replace(/[TZ]/g, " ");
+}
 </script>
 
 <template>
-  <div>
-    <BlogPost
-      v-for="post in posts"
-      :key="post.id"
-      :title="post.title"
-      @enlarge-text="postFontSize += 0.1"
-      :style="{ fontSize: postFontSize + 'em' }"
-    >
-      slot test {{ post.id }}
-    </BlogPost>
-  </div>
-  <hr />
-  <table>
-    <BlogPost key="44" title="tr"></BlogPost>
-    <tr is="vue:BlogPost" key="44" title="tr"></tr>
-  </table>
+  <h1>Latest Vue Core Commits</h1>
+  <template v-for="branch in branches">
+    <input
+      type="radio"
+      :id="branch"
+      :value="branch"
+      name="branch"
+      v-model="currentBranch"
+    />
+    <label :for="branch">{{ branch }}</label>
+  </template>
+  <p>vuejs/vue@{{ currentBranch }}</p>
+  <ul>
+    <li v-for="{ html_url, sha, author, commit } in commits">
+      <a :href="html_url" target="_blank" class="commit">{{
+        sha.slice(0, 7)
+      }}</a>
+      - <span class="message">{{ truncate(commit.message) }}</span
+      ><br />
+      by
+      <span class="author">
+        <a :href="author.html_url" target="_blank">{{ commit.author.name }}</a>
+      </span>
+      at <span class="date">{{ formatDate(commit.author.date) }}</span>
+    </li>
+  </ul>
 </template>
+
+<style>
+a {
+  text-decoration: none;
+  color: #42b883;
+}
+li {
+  line-height: 1.5em;
+  margin-bottom: 20px;
+}
+.author,
+.date {
+  font-weight: bold;
+}
+</style>
---
+++ vue-demo/src/AppSlot.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+import BlogPost from "@/components/BlogPost.vue";
+
+const posts = ref([
+  { id: 1, title: "My journey with Vue" },
+  { id: 2, title: "Blogging with Vue" },
+  { id: 3, title: "Why Vue is so fun" },
+]);
+
+const postFontSize = ref(1);
+</script>
+
+<template>
+  <div>
+    <BlogPost
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      @enlarge-text="postFontSize += 0.1"
+      :style="{ fontSize: postFontSize + 'em' }"
+    >
+      slot test {{ post.id }}
+    </BlogPost>
+  </div>
+  <hr />
+  <table>
+    <BlogPost key="44" title="tr"></BlogPost>
+    <tr is="vue:BlogPost" key="44" title="tr"></tr>
+  </table>
+</template>
