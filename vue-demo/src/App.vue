67d5d626439be64de870ac33cec89dd788a2d4c5
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-11 21:16:03 +0800

    sort_filter

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,80 +1,23 @@
 <!--
-这个示例会通过 GitHub 的 API 获取最新的 Vue.js 提交信息并将其展示为列表。
-你可以在两个分支之间切换。
+该示例创建了一个可复用网格组件，并结合外部数据使用它。
 -->
 
 <script setup>
-import { ref, watchEffect } from "vue";
+import GridCom from "@/components/GridCom.vue";
+import { ref } from "vue";
 
-const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`;
-const branches = ["main", "v2-compat"];
-
-const currentBranch = ref(branches[0]);
-const commits = ref(null);
-watchEffect(async () => {
-  // 该 effect 会立即运行，
-  // 并且在 currentBranch.value 改变时重新运行
-  const url = `${API_URL}${currentBranch.value}`;
-  commits.value = JSON.parse(
-    await fetch(url).then(function (data) {
-      // text()方法属于fetchAPI的一部分，它返回一个Promise实例对象，
-      // 用于获取后台返回的数据 return data.text();
-      return data.text();
-    })
-  );
-  console.log(commits.value === (await (await fetch(url)).json()));
-});
-
-function truncate(v) {
-  const newline = v.indexOf("\n");
-  return newline > 0 ? v.slice(0, newline) : v;
-}
-
-function formatDate(v) {
-  return v.replace(/[TZ]/g, " ");
-}
+const searchQuery = ref("");
+const gridColumns = ["name", "power"];
+const gridData = [
+  { name: "Chuck Norris", power: Infinity },
+  { name: "Bruce Lee", power: 9000 },
+  { name: "Jackie Chan", power: 7000 },
+  { name: "Jet Li", power: 8000 },
+];
 </script>
 
 <template>
-  <h1>Latest Vue Core Commits</h1>
-  <template v-for="branch in branches">
-    <input
-      type="radio"
-      :id="branch"
-      :value="branch"
-      name="branch"
-      v-model="currentBranch"
-    />
-    <label :for="branch">{{ branch }}</label>
-  </template>
-  <p>vuejs/vue@{{ currentBranch }}</p>
-  <ul>
-    <li v-for="{ html_url, sha, author, commit } in commits">
-      <a :href="html_url" target="_blank" class="commit">{{
-        sha.slice(0, 7)
-      }}</a>
-      - <span class="message">{{ truncate(commit.message) }}</span
-      ><br />
-      by
-      <span class="author">
-        <a :href="author.html_url" target="_blank">{{ commit.author.name }}</a>
-      </span>
-      at <span class="date">{{ formatDate(commit.author.date) }}</span>
-    </li>
-  </ul>
+  <form id="search">Search <input name="query" v-model="searchQuery" /></form>
+  <GridCom :data="gridData" :columns="gridColumns" :filter-key="searchQuery">
+  </GridCom>
 </template>
-
-<style>
-a {
-  text-decoration: none;
-  color: #42b883;
-}
-li {
-  line-height: 1.5em;
-  margin-bottom: 20px;
-}
-.author,
-.date {
-  font-weight: bold;
-}
-</style>
