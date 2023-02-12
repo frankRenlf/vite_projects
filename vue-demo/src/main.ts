4176b763f8ef98ab2d0a2fe0716af6a303832670
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-12 12:02:21 +0800

    Update main.ts

--- vue-demo/src/main.ts
+++ vue-demo/src/main.ts
@@ -11,7 +11,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 // app.use(router);
-
+// app.component("TodoItem", TodoItem);
 app.mount("#app");
 // console.log(app.config);
-// app.component("TodoItem", TodoItem);
