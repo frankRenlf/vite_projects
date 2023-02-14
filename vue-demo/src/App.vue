d69f605abdccf0cfc6fba2b0b4afe8c4a634a4ba
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-14 10:06:42 +0800

    update

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,13 +1,15 @@
-
 <script setup lang="ts">
-
 import SlotCom from "@/components/SlotCom.vue";
+import { useMouse } from "@/hooks/mouse";
+let { x, y } = useMouse();
 </script>
 
 <template>
   <SlotCom>
-    <template #test>
-      test #test
+    <template #item="item">
+      <p>{{ item.id }}</p>
+      <p>by {{ item.address }}</p>
     </template>
   </SlotCom>
+  <div>{{ x }}--{{ y }}</div>
 </template>
--- vue-demo/src/components/SlotCom.vue
+++ vue-demo/src/components/SlotCom.vue
@@ -1,14 +1,19 @@
 <template>
-  <slot name="test" :propsData="props"> default </slot>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      // slot transfer props must use v-bind=""
+      <slot name="item" v-bind="item"></slot>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
 import { reactive } from "vue";
 
-const props = reactive({
-  age: 11,
-  address: "hz",
-});
+const items = reactive([
+  { id: 0, address: "hz" },
+  { id: 2, address: "sh" },
+]);
 </script>
 
 <style scoped></style>
