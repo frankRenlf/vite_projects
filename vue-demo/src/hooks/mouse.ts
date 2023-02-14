ba2f9ca3b123500d8e7dda373ae02a0b8f461463
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-14 14:03:52 +0800

    Update mouse.ts

--- vue-demo/src/hooks/mouse.ts
+++ vue-demo/src/hooks/mouse.ts
@@ -1,7 +1,7 @@
 import { ref, onMounted, onUnmounted } from "vue";
 
 // 按照惯例，组合式函数名以“use”开头
-export function useMouse() {
+export const useMouse = () => {
   // 被组合式函数封装和管理的状态
   const x = ref(0);
   const y = ref(0);
@@ -14,9 +14,14 @@ export function useMouse() {
 
   // 一个组合式函数也可以挂靠在所属组件的生命周期上
   // 来启动和卸载副作用
-  onMounted(() => window.addEventListener("mousemove", update));
+  onMounted(() => {
+    window.addEventListener("mousemove", update);
+    window.addEventListener("mousedown", () => {
+      console.log("point: X", x.value, "Y", y.value);
+    });
+  });
   onUnmounted(() => window.removeEventListener("mousemove", update));
 
   // 通过返回值暴露所管理的状态
   return { x, y };
-}
+};
