88ede0317692386ea4e8354731fe26ba549ea5ce
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-14 10:16:14 +0800

    Update SlotCom.vue

--- vue-demo/src/components/SlotCom.vue
+++ vue-demo/src/components/SlotCom.vue
@@ -1,7 +1,7 @@
 <template>
   <ul>
+    slot transfer props must use v-bind=""
     <li v-for="item in items" :key="item.id">
-      // slot transfer props must use v-bind=""
       <slot name="item" v-bind="item"></slot>
     </li>
   </ul>
