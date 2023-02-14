2bf635728ba129be5c1563c1866ccd54602cd3ec
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-14 14:32:32 +0800

    update

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -2,6 +2,15 @@
 import SlotCom from "@/components/SlotCom.vue";
 import { useMouse } from "@/hooks/mouse";
 let { x, y } = useMouse();
+
+import { ref, computed } from "vue";
+import { useFetch } from "@/hooks/useFetch.js";
+
+const baseUrl = "https://jsonplaceholder.typicode.com/todos/";
+const id = ref("1");
+const url = computed(() => baseUrl + id.value);
+
+const { data, error, retry } = useFetch(url);
 </script>
 
 <template>
@@ -12,4 +21,13 @@ let { x, y } = useMouse();
     </template>
   </SlotCom>
   <div>{{ x }}--{{ y }}</div>
+
+  Load post id:
+  <button v-for="i in 5" :key="i" @click="id = i">{{ i }}</button>
+  <div v-if="error">
+    <p>Oops! Error encountered: {{ error.message }}</p>
+    <button @click="retry">Retry</button>
+  </div>
+  <div v-else-if="data">Data loaded: <pre>{{ data }}</pre></div>
+  <div v-else>Loading...</div>
 </template>
---
+++ vue-demo/src/hooks/useFetch.ts
@@ -0,0 +1,52 @@
+import { ref, isRef, unref, watchEffect } from "vue";
+
+export function useFetch(url: any) {
+  const data = ref<object>();
+  const error = ref<object>();
+
+  async function doFetch() {
+    // reset state before fetching..
+    // data.value = null;
+    // error.value = null;
+
+    // resolve the url value synchronously, so it's tracked as a
+    // dependency by watchEffect()
+    const urlValue = unref(url);
+
+    try {
+      // artificial delay / random error
+      await timeout();
+      // unref() will return the ref value if it's a ref
+      // otherwise the value will be returned as-is
+      const res = await fetch(urlValue);
+      data.value = await res.json();
+    } catch (e: any) {
+      error.value = e;
+    }
+  }
+
+  if (isRef(url)) {
+    // setup reactive re-fetch if input URL is a ref
+    watchEffect(doFetch);
+  } else {
+    // otherwise, just fetch once
+    doFetch();
+  }
+
+  return { data, error, retry: doFetch };
+}
+
+// artificial delay
+function timeout() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const val = Math.random();
+      console.log(val);
+      if (val > 0.3) {
+        resolve("Success");
+      } else {
+        reject(new Error("Random Error"));
+      }
+    }, 300);
+  });
+}
