16c9508c8da715f1f4be4a9aa4236d8f478e1ae2
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-15 16:59:23 +0800

    Update App.vue

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -3,9 +3,17 @@ import {ref, shallowRef} from "vue";
 import App_transitionGroup2 from "@/App_transitionGroup2.vue";
 import App_transitionGroup1 from "@/App_transitionGroup1.vue";
 const cur = shallowRef(App_transitionGroup1);
+let open = ref(false);
 </script>
 
 <template>
+  <button @click="open = true">Open Modal</button>
+  <Teleport to="body">
+    <div v-if="open" class="modal">
+      <p>Hello from the modal!</p>
+      <button @click="open = false">Close</button>
+    </div>
+  </Teleport>
   <input type="radio" v-model="cur" :value="App_transitionGroup1" />
   <input type="radio" v-model="cur" :value="App_transitionGroup2" />
   <keep-alive :include="['App_transitionGroup2']">
