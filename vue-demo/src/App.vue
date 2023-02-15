2f46105618a61f34ee75395c408eee84acea8f4b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-15 14:56:28 +0800

    update

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,24 +1,82 @@
-<script setup lang="ts">
-import { ref } from "vue";
+<!--
+通过内建的 <TransitionGroup> 实现“FLIP”列表过渡效果。
+https://aerotwist.com/blog/flip-your-animations/
+-->
 
-let display = ref(false);
+<script setup lang="ts">
+// import { shuffle as _shuffle } from "lodash-es";
+import { ref } from "vue";
+import { shuffle } from "lodash-es";
+
+const getInitialItems = () => [1, 2, 3, 4, 5];
+const items = ref(getInitialItems());
+let id = items.value.length + 1;
+
+function insert() {
+  const i = Math.round(Math.random() * items.value.length);
+  items.value.splice(i, 0, id++);
+}
+
+function reset() {
+  items.value = getInitialItems();
+}
+
+function myShuffle() {
+  items.value = shuffle(items.value);
+}
+
+function remove(item: any) {
+  const i = items.value.indexOf(item);
+  if (i > -1) {
+    items.value.splice(i, 1);
+  }
+}
 </script>
 
 <template>
-  <button @click="display = !display">dispaly</button>
-  <transition>
-    <div v-show="display">{{ $translate("greetings.hi") }}</div>
-  </transition>
+  <button @click="insert">insert at random index</button>
+  <button @click="reset">reset</button>
+  <button @click="myShuffle">shuffle</button>
+
+  <TransitionGroup tag="ul" name="fade" class="container">
+    <div v-for="item in items" class="item" :key="item">
+      {{ item }}
+      <button @click="remove(item)">x</button>
+    </div>
+  </TransitionGroup>
 </template>
 
-<style lang="less">
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
+<style>
+.container {
+  position: relative;
+  padding: 0;
 }
 
-.v-enter-from,
-.v-leave-to {
+.item {
+  width: 100%;
+  height: 30px;
+  background-color: #f3f3f3;
+  border: 1px solid #666;
+  box-sizing: border-box;
+}
+
+/* 1. 声明过渡效果 */
+.fade-move,
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+/* 2. 声明进入和离开的状态 */
+.fade-enter-from,
+.fade-leave-to {
   opacity: 0;
+  transform: scaleY(0.01) translate(30px, 0);
+}
+
+/* 3. 确保离开的项目被移除出了布局流
+      以便正确地计算移动时的动画效果。 */
+.fade-leave-active {
+  position: absolute;
 }
 </style>
---
+++ vue-demo/src/App_plugin.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+let display = ref(false);
+</script>
+
+<template>
+  <button @click="display = !display">dispaly</button>
+  <transition name="display">
+    <div v-show="display">{{ $translate("greetings.hi") }}</div>
+  </transition>
+</template>
+
+<style lang="less">
+.display-enter-active,
+.display-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.display-enter-from,
+.display-leave-to {
+  opacity: 0;
+}
+</style>
