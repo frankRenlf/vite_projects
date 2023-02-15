49aba5bbb0c6f2058a4e7529f5256b4d7739e225
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-15 13:11:25 +0800

    Update i18n.ts

--- vue-demo/src/plugin/i18n.ts
+++ vue-demo/src/plugin/i18n.ts
@@ -5,11 +5,11 @@ export default {
     app.config.globalProperties.$translate = (key: string) => {
       // 获取 `options` 对象的深层属性
       // 使用 `key` 作为索引
-      console.log(key.split("."));
+      console.log("array", key.split("."));
       return key.split(".").reduce((o, i) => {
-        console.log(o, i);
+        console.log("before", o, i);
         if (o) {
-          console.log("ret", o, i);
+          // console.log(o, i, o[i]);
           return o[i];
         }
       }, options);
