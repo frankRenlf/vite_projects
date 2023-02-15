7f353d29a114b99f894ab3537aa31b703cf62162
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-15 14:19:58 +0800

    Update App.vue

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-<div>{{ $translate('greetings.hi') }}</div>
+  <transition>
+    <div>{{ $translate("greetings.hi") }}</div>
+  </transition>
 </template>
+
+<style lang="less">
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
