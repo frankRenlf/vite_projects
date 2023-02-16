efbd1dda5b857419ed5337969449b0b7087a2433
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-16 19:14:07 +0800

    Update App.vue

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -8,10 +8,33 @@ const uc = useCounterStore();
 </script>
 
 <template>
+  <ul class="skip-links">
+    <li>
+      <a href="#main" ref="skipLink" class="skip-link">Skip to main content</a>
+    </li>
+  </ul>
+
   <pinia-test></pinia-test>
   <div>{{ uc.count }}</div>
   <SlotCom>
-    <template #item="item1"> {{ item1.id }}  </template>
+    <template #item="item1"> {{ item1.id }} </template>
   </SlotCom>
   <AppFetch></AppFetch>
 </template>
+
+<style>
+.skip-link {
+  white-space: nowrap;
+  top: 0;
+  position: fixed;
+  left: 50%;
+  margin: 1em auto 1em -72px;
+  opacity: 0;
+}
+.skip-link:hover {
+  opacity: 1;
+  background-color: white;
+  padding: 0.5em;
+  border: 1px solid black;
+}
+</style>
